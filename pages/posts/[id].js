fc4507c7033f9f29d15f18ceeaccebec4c354e5a
wayyt <huangruofeng33@gmail.com>
2023-03-22 18:52:19 +0800

    update:first-update

---
+++ pages/posts/[id].js
@@ -0,0 +1,43 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/post";
+// Add this import
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.scss";
+import { useRouter } from "next/router";
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+//q:如果这个页面是ssr方式用什么api?
+//a: getServerSideProps
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: true,
+  };
+}
+export default function Post({ postData }) {
+  //   const router = useRouter();
+  //   console.log(router);
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
